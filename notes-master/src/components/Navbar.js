63ef0fb01dddbce8418a7b8a116500f6c4f47573
Parv Shah <46294393+go4amitshah@users.noreply.github.com>
2023-04-28 18:08:19 +0530

    React Incorporated

---
+++ notes-master/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="container">
+        <div className="navbar navbar-expand-lg">
+            <div className="navbar-brand">notesMasters!</div>
+        </div>
+      </div>
+    </div>
+  )
+}
